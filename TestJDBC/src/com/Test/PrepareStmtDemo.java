115ec85a16685247272efc1964376c2957b7eeb8
Shubhangi-Shelke <shubhangishelke9272@gmail.com>
2026-06-05 17:14:40 +0530

    Prepared Statement

---
+++ TestJDBC/src/com/Test/PrepareStmtDemo.java
@@ -0,0 +1,41 @@
+package com.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class PrepareStmtDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+
+			// step -1 Register driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// step-2 Create Connection
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_db ", "root", "root");
+
+			// step =3 Creatye Statement
+			PreparedStatement pst = con.prepareStatement("insert into person values(?,?,?)");
+			pst.setInt(1, 102);
+			pst.setString(2, "Mayuri");
+			pst.setInt(3, 70);
+
+			// Step- 4 Excecute Query
+
+			int i = pst.executeUpdate();
+			System.out.println(i + "Data Inserted Successfully");
+
+			// step = 5 close Connection
+			con.close();
+
+		} catch (Exception e) {
+
+			System.out.println(e);
+		}
+
+	}
+
+}
